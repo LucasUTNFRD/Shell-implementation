dcd68533c766cec95a12b6ff17433c622582da59
LucasUTNFRD <ldelgado@frd.utn.edu.ar>
2024-05-15 22:27:58 -0300

    refactoring shell with better parsing

--- shell.c
+++ shell.c
@@ -1,181 +1,131 @@
+#include "shell.h"
+// #include <cstddef>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
+#include <unistd.h>
+#include <wait.h>
 
-#define MAX_COMMAND_LENGTH 100
-#define MAX_ARGUMENTS 10
+#define EXIT_SHELL 1
 
-// Struct to store information about background jobs
-typedef struct {
-    pid_t pid;
-    int job_id;
-    char command[MAX_COMMAND_LENGTH];
-} BackgroundJob;
+char prompt[PROMPT_LEN];
 
-BackgroundJob background_jobs[MAX_ARGUMENTS];
-int num_background_jobs = 0;
+static char buffer[BUFF_LEN];
 
-void sigchld_handler(int signum) ;
+char *read_line(const char *prompt) {
+  int i = 0;
+  int c = 0;
+  memset(buffer, 0, BUFF_LEN);
 
-void sigint_handler(int signum) ;
-char *read_input() ;
-char **tokenize_input(char *input, int *arg_size) ;
-void exec_cd(char **args);
-void exec_jobs(char **args);
+  c = getchar();
+  while (c != END_LINE && c != EOF) {
+    buffer[i++] = c;
+    c = getchar();
+  }
 
-void execute_builtin(char **argv) ;
-typedef void (*shell_func)(char **args);
+  if (c == EOF) {
+    return NULL;
+  }
 
-shell_func shell_funcs[] = {&exec_cd, &exec_jobs};
+  buffer[i] = END_STRING;
 
-char *builtin[] = {"cd", "jobs"};
-
-// Signal handler for SIGCHLD (child process termination)
-void sigchld_handler(int signum) {
-    pid_t pid;
-    int status;
-    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
-        int i;
-        for (i = 0; i < num_background_jobs; ++i) {
-            if (background_jobs[i].pid == pid) {
-                printf("[%d] Done\t%s\n", background_jobs[i].job_id, background_jobs[i].command);
-                // Remove the completed job from the list
-                memmove(&background_jobs[i], &background_jobs[i + 1], (num_background_jobs - i - 1) * sizeof(BackgroundJob));
-                num_background_jobs--;
-                break;
-            }
-        }
-    }
+  return buffer;
 }
 
-void sigint_handler(int signum){
-    if (num_background_jobs != 0) {
-        printf("sig_int(%d):background process pending, cant exit\n", signum);
-    }else{
-//        free(background_jobs);
-        printf("sig_int(%d): ended!\n",signum);
-        exit(0);
-    }
+Command *create_command(char *name, char **args, int num_args, int background) {
+  Command *cmd = malloc(sizeof(Command));
+  if (cmd) {
+    cmd->name = strdup(name);
+    cmd->args = args;
+    cmd->num_args = num_args;
+    cmd->background = background;
+  }
 
+  return cmd;
 }
 
-// Execute commands
-void execute_command(char **args, int background) {
-    pid_t pid = fork();
-    if (pid < 0) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        // Child process
-        if (execvp(args[0], args) == -1) {
-            perror("execvp");
-            exit(EXIT_FAILURE);
-        }
-    } else {
-        // Parent process
-        if (!background) {
-            // Wait for foreground process to finish
-            waitpid(pid, NULL, 0);
-        } else {
-            // Background process
-            BackgroundJob job = {pid, num_background_jobs + 1, ""};
-            strncpy(job.command, args[0], MAX_COMMAND_LENGTH);
-            background_jobs[num_background_jobs++] = job;
-            printf("[%d] %d\t%s\n", job.job_id, job.pid, job.command);
-        }
-    }
+void print_command(Command *cmd) {
+  printf("Command name:%s\n", cmd->name);
+  printf("Num of args: %d\n", cmd->num_args);
+  // for (int i = 0; i < cmd->num_args; i++) {
+  //   printf("args[%d] %s\n", i, cmd->args[i]);
+  // }
+  printf("is background: %d\n", cmd->background);
 }
 
-char *read_input() {
-    char *buffer;
-    size_t bufsize = 32;
-    ssize_t characters;
-
-    buffer = (char *)malloc(bufsize * sizeof(char));
-    if (buffer == NULL) {
-        perror("Unable to allocate buffer");
-        exit(1);
-    }
-
-    printf("> ");
-    characters = getline(&buffer, &bufsize, stdin);
-    return buffer;
+void free_command(Command *cmd) {
+  if (cmd) {
+    free(cmd->name);
+    free(cmd->args); // Free memory for args array
+    free(cmd);
+  }
 }
 
-char **tokenize_input(char *input, int *arg_size) {
-    int i = 0;
-    char **args = (char **)malloc((MAX_ARGUMENTS + 1) * sizeof(char *));
-    if (args == NULL) {
-        perror("Unable to allocate memory for arguments");
-        exit(1);
-    }
-
-    args[i] = strtok(input, " \n");
-    while (args[i] != NULL && i < MAX_ARGUMENTS) {
-        i++;
-        args[i] = strtok(NULL, " \n");
-    }
-    args[i] = NULL;
-    *arg_size = i;
-    return args;
+char **tokenize_prompt(char *prompt, int *num_args) {
+  int i = 0;
+  char **args = (char **)malloc((MAXARGS + 1) * sizeof(char *));
+  if (args == NULL) {
+    perror("malloc failed\n");
+    exit(1);
+  }
+  char *token = strtok(prompt, DELIM);
+  while (token != NULL && i < MAXARGS) {
+    args[i] = token;
+    // printf("token traced: %s\n", args[i]);
+    i++;
+    token = strtok(NULL, DELIM);
+  }
+  args[i] = NULL;
+  *num_args = i;
+  return args;
 }
 
-void exec_cd(char **argv) {
-    if (argv[1] == NULL) {
-        fprintf(stderr, "cd: missing argument\n");
-        return;
-    }
-    if (chdir(argv[1]) != 0) {
-        perror("cd error");
-    }
+Command *parse_line(char *prompt) {
+  int num_args;
+  char **args = tokenize_prompt(prompt, &num_args);
+  char *name = args[0];
+  Command *cmd = create_command(name, args, num_args, 0);
+  return cmd;
 }
 
-void exec_jobs(char **argv) {
-    // List background jobs
-    int j;
-    for (j = 0; j < num_background_jobs; ++j) {
-        printf("[%d] %d\t%s\n", background_jobs[j].job_id, background_jobs[j].pid, background_jobs[j].command);
-    }
-}
-
-void execute_builtin(char **argv) {
-    size_t len = sizeof(builtin) / sizeof(char *);
-    for (size_t i = 0; i < len; i++) {
-        if (strcmp(argv[0], builtin[i]) == 0) {
-            shell_funcs[i](argv);
-            return;
-        }
-    }
-}
-
-int main() {
-    char *input;
-    char **args;
-    int arg_size;
-    signal(SIGCHLD, sigchld_handler);
-    signal(SIGINT,sigint_handler);
-    while (1) {
-        input = read_input();
-        args = tokenize_input(input, &arg_size);
-
-        execute_builtin(args);
-
-        int background = 0;
-
-        if (arg_size > 0 && strcmp(args[arg_size - 1], "&") == 0) {
-            // Background job
-            background = 1;
-            args[arg_size - 1] = NULL;
-            arg_size--; // Update argument count
-        }
-        execute_command(args, background);
-
-        free(input);
-        free(args);
-    }
+int run_command(char *cmd) {
+  if (strlen(cmd) == 0) {
     return 0;
+  }
+
+  Command *command = parse_line(cmd);
+
+  pid_t pid = fork();
+  if (pid < 0) {
+    perror("fork");
+    return -1;
+  } else if (pid == 0) {
+    // child process
+    // exectute the Command
+    if (execvp(command->name, command->args) == -1) {
+      perror("execvp");
+      exit(EXIT_FAILURE);
+    }
+  } else {
+    if (!command->background) {
+      int status;
+      waitpid(pid, &status, 0);
+    }
+  }
+  free_command(command);
+  return 0;
+}
+
+static void run_shell() {
+  char *cmd;
+  while ((cmd = read_line(prompt)) != NULL) {
+    if (run_command(cmd) == EXIT_SHELL)
+      return;
+  }
+}
+
+int main(void) {
+  run_shell();
+  return 0;
 }
---
+++ shell.h
@@ -0,0 +1,31 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#define PROMPT_LEN 1024
+#define BUFF_LEN 1024
+#define END_LINE '\n'
+#define END_STRING '\0'
+#define MAXARGS 20
+#define DELIM " \n"
+
+typedef struct {
+  char *name;   // Command name
+  char **args;  // Command arguments (including command name itself)
+  int num_args; // Number of arguments
+  // char *input_redirect;  // Input file redirection (if any)
+  // char *output_redirect; // Output file redirection (if any)
+  int background; // Flag indicating if the command should run in the background
+} Command;
+
+Command *create_command(char *name, char **args, int num_args, int background);
+
+Command *parse_line(char *prompt);
+
+char **tokenize_prompt(char *prompt, int *num_args);
+
+void free_command(Command *cmd);
+
+char *read_line(const char *prompt);
+int run_command(char *cmd);
+
+#endif // !SHELL_H
